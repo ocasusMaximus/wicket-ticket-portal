252981ce6580d2062d1a8dc15a4c17e6ceaf2ad9
ocasusMaximus <za.jan@seznam.cz>
2020-07-22 12:44:09 +0200

    Hsqldb connect, added pages with css changes, wicket logo and favicon add, first functional form

--- src/main/java/fim/pages/AddTicket.java
+++ src/main/java/fim/pages/AddTicket.java
@@ -3,8 +3,9 @@ package fim.pages;
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import org.apache.wicket.markup.html.WebPage;
 
-@WicketHomePage
-public class HomePage extends WebPage {
+
+public class AddTicket extends WebPage {
+
 }
 
 
---
+++ src/main/java/fim/pages/Tickets.java
@@ -0,0 +1,12 @@
+package fim.pages;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+
+
+@WicketHomePage
+public class Tickets extends WebPage {
+
+}
+
+
